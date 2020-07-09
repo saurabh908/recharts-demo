b1e93053574d93942ccbe915d33dda9edb732fcf
Saurabh Kumar <34212933+saurabh908@users.noreply.github.com>
2020-07-09 19:56:59 +0530

    composed chart

--- src/components/Rechart.jsx
+++ src/components/Rechart.jsx
@@ -7,77 +7,61 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend
+  Area,
+  Legend,
+  Line
 } from "recharts";
 import { Card } from "react-bootstrap";
 
 const data = [
   {
     name: "Fri",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
+    complete: 50,
+    urgent: 14
   },
   {
     name: "Sat",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
+    complete: 40,
+    urgent: 25
   },
   {
     name: "Sun",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
+    complete: 20,
+    urgent: 10
   },
   {
     name: "Mon",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
+    complete: 70,
+    urgent: 30
   },
   {
     name: "Tue",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
+    complete: 60,
+    urgent: 40
   },
   {
     name: "Wed",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
+    complete: 80,
+    urgent: 50
   },
   {
     name: "Today",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
+    complete: 50,
+    urgent: 14
   }
 ];
 
 const MixedBarChart = () => {
   return (
     <div>
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        // margin={{
-        //   top: 20,
-        //   right: 30,
-        //   left: 20,
-        //   bottom: 5
-        // }}
-      >
+      <BarChart width={400} height={300} data={data}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
-        <YAxis />
+        {/* <YAxis /> */}
         <Tooltip />
-        <Legend />
-        <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-        <Bar dataKey="amt" stackId="a" fill="#82ca9d" />
-        <Bar dataKey="uv" fill="#ffc658" />
+        {/* <Legend /> */}
+        <Bar dataKey="complete" fill="rgba(55, 146, 203,1)" />
+        <Bar dataKey="urgent" fill="rgba(242,115,96,1)" />
       </BarChart>
     </div>
   );
@@ -85,25 +69,34 @@ const MixedBarChart = () => {
 
 const StackedBarChart = () => {
   return (
-    <BarChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{
-        top: 20,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
+    <BarChart width={400} height={300} data={data}>
       <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      {/* <YAxis /> */}
+      <Tooltip />
+      {/* <Legend /> */}
+      <Bar dataKey="complete" stackId="a" fill="rgba(55, 146, 203,1)" />
+      <Bar dataKey="urgent" stackId="a" fill="rgba(242,115,96,1)" />
+    </BarChart>
+  );
+};
+
+const ComposedChart = () => {
+  return (
+    <ComposedChart width={400} height={300} data={data}>
+      <CartesianGrid stroke="#f5f5f5" />
       <XAxis dataKey="name" />
       <YAxis />
       <Tooltip />
       <Legend />
-      <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-      <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
-    </BarChart>
+      {/* <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" /> */}
+      {/* <Bar dataKey="complete" barSize={20} fill="#413ea0" /> */}
+
+      <Bar dataKey="complete" fill="rgba(55, 146, 203,1)" />
+      <Bar dataKey="urgent" fill="rgba(242,115,96,1)" />
+      {/* <Line type="monotone" dataKey="uv" stroke="#ff7300" /> */}
+      {/* <Scatter dataKey="cnt" fill="red" /> */}
+    </ComposedChart>
   );
 };
 
@@ -111,17 +104,24 @@ export default class Rechart extends PureComponent {
   render() {
     return (
       <div>
-        <Card style={{ width: "35rem", height: "30rem" }}>
-          <Card.Header>{this.props.barTitle}</Card.Header>
+        <Card style={{ width: "28rem", height: "24rem" }}>
+          <Card.Header>Completed and Urgent Tasks</Card.Header>
           <Card.Body>
             <MixedBarChart />
           </Card.Body>
         </Card>
-
-        <Card style={{ width: "35rem", height: "30rem" }}>
-          <Card.Header>{this.props.barTitle}</Card.Header>
+        &nbsp;
+        <Card style={{ width: "28rem", height: "24rem" }}>
+          <Card.Header>Completed and Urgent Stacked Tasks</Card.Header>
           <Card.Body>
             <StackedBarChart />
+          </Card.Body>
+        </Card>
+        &nbsp;
+        <Card style={{ width: "28rem", height: "24rem" }}>
+          <Card.Header>Composed Completed and Urgent Tasks</Card.Header>
+          <Card.Body>
+            <ComposedChart />
           </Card.Body>
         </Card>
       </div>
